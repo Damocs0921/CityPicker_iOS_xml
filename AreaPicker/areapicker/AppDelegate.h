b7fca7643ad171399902d693a00edb0b7128f27b
DamocsYang <damocs0921@foxmail.com>
2015-06-11 19:04:33 +0800

    中国大陆城市地区选择器，支持省市区三级与省市二级两种    对@Cloud Dai 2012年版本的ARC扩展，新增xml格式的本地数据源的读取与解析

---
+++ AreaPicker/areapicker/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  areapicker
+//
+//  Created by Damocs Yang on 15-6-11.
+//  Copyright (c) 2015年 damocs.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
